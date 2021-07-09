06d1fcd1e219c824bceb6009649962deffd49a04
xiaqiao <156751121@qq.com>
2021-07-09 18:02:25 +0800

    -init

---
+++ micro-service-order/src/main/java/cn/x/microservice/order/config/RabbitMqConfig.java
@@ -0,0 +1,12 @@
+package cn.x.microservice.order.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xqa
+ * @since 2021/7/9
+ */
+@Configuration
+public class RabbitMqConfig {
+
+}
